d8a8f351920f589652f91a3cfb6f70f18ece8557
alexgr15 <alex_gr15@hotmail.com>
2025-12-25 21:18:21 -0300

    FIX: Remove nested AppShell causing Double Sidebar

--- src/components/Editor/EditorLayout.jsx
+++ src/components/Editor/EditorLayout.jsx
@@ -1,4 +1,3 @@
-import { AppShell } from '../Layout/AppShell';
 import { Scene } from '../Viewer3D/Scene';
 import { ProductionCanvas } from '../Editor2D/ProductionCanvas';
 import { NestingView } from '../Production/NestingView';
@@ -8,34 +7,28 @@ export const EditorLayout = () => {
     const { activeTool } = useDesignStore();
 
     if (activeTool === 'nesting') {
-        return (
-            <AppShell>
-                <NestingView />
-            </AppShell>
-        );
+        return <NestingView />;
     }
 
     return (
-        <AppShell>
-            <div style={{
-                width: '100%',
-                height: '100%',
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr',
-                background: '#000'
-            }}>
-                {/* 2D Editor Area */}
-                <div style={{ position: 'relative', borderRight: 'var(--glass-border)' }}>
-                    <div style={{ position: 'absolute', top: 10, left: 10, color: 'white', zIndex: 10, pointerEvents: 'none' }}>2D Editor</div>
-                    <ProductionCanvas />
-                </div>
-
-                {/* 3D Preview Area */}
-                <div style={{ position: 'relative' }}>
-                    <div style={{ position: 'absolute', top: 10, left: 10, color: 'white', zIndex: 10, pointerEvents: 'none' }}>3D Preview</div>
-                    <Scene />
-                </div>
+        <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+            background: '#000'
+        }}>
+            {/* 2D Editor Area */}
+            <div style={{ position: 'relative', borderRight: 'var(--glass-border)' }}>
+                <div style={{ position: 'absolute', top: 10, left: 10, color: 'white', zIndex: 10, pointerEvents: 'none' }}>2D Editor</div>
+                <ProductionCanvas />
             </div>
-        </AppShell>
+
+            {/* 3D Preview Area */}
+            <div style={{ position: 'relative' }}>
+                <div style={{ position: 'absolute', top: 10, left: 10, color: 'white', zIndex: 10, pointerEvents: 'none' }}>3D Preview</div>
+                <Scene />
+            </div>
+        </div>
     );
 };
